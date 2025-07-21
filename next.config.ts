6b50ba2fdf6de59892f8b1aee489f7bf40181e25
heshamwbadr <hesham.w.badr@gmail.com>
2025-07-21 16:09:03 +0200

    mobile

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  productionBrowserSourceMaps: true,
   // Modern browser optimization
   experimental: {
     // Enable modern JavaScript features
